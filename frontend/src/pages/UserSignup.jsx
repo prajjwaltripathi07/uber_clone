e2bc32e09b17235dd138d98b007d538ded134ac9
PRAJJWAL TRIPATHI <prajjwaltripathi70@gmail.com>
2025-03-03 16:51:33 +0530

    auth done

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,50 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import  UserContext  from '../context/UserContext'
 import { UserDataContext } from '../context/UserContext'
+
+
 
 const UserSignup = () => {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ firstName, setFirstName ] = useState('')
   const [ lastName, setLastName ] = useState('')
-   const [ userData, setUserData ] = useState('')
+  const [ userData, setUserData ] = useState({})
 
   const navigate = useNavigate()
 
-  const{user,setUser}=React.useContext(UserContext)
 
 
-  const submitHandler =  async(e) => {
+  const { user, setUser } = useContext(UserDataContext)
+
+
+
+
+  const submitHandler = async (e) => {
     e.preventDefault()
-    const newUser={
-        fullName: {
-          firstname: firstName,
-          lastname: lastName
-        },
-        email: email,
-        password: password
+    const newUser = {
+      fullname: {
+        firstname: firstName,
+        lastname: lastName
+      },
+      email: email,
+      password: password
     }
 
-const response=await axios.post(`${import.meta.env.VITE_BASE_URL}/user/register`,newUser)
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
 
-if(response.status===201)
-{ 
-  const data=await response.data
+    if (response.status === 201) {
+      const data = response.data
+      setUser(data.user)
+      localStorage.setItem('token', data.token)
+      navigate('/home')
+    }
 
-  setUser(data.user)
 
-  
-  navigate('/home')
-}
-
-    console.log(userData)
     setEmail('')
     setFirstName('')
     setLastName('')
     setPassword('')
+
   }
   return (
     <div>
