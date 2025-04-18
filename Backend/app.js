cf7e811d285f08d8b09c99c35ab8ce2836b7fa4d
PRAJJWAL TRIPATHI <prajjwaltripathi70@gmail.com>
2025-04-18 09:23:56 +0530

    app.js changed

--- Backend/app.js
+++ Backend/app.js
@@ -12,22 +12,9 @@ const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.use('/users', userRoutes);
-app.use('/captains', captainRoutes);
-app.use('/maps', mapsRoutes);
-app.use('/rides', rideRoutes);
-
 
 const corsOptions = {
     origin: ['https://uber-clone-jet-nine.vercel.app'], // Replace with your frontend's deployed URL
@@ -37,6 +24,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
