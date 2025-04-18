246db4f12de489741ee70a0669c442a6dacdd2e7
PRAJJWAL TRIPATHI <2k22.csaiml.2212855@gmail.com>
2025-04-18 09:16:12 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -27,6 +27,11 @@ app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapsRoutes);
 app.use('/rides', rideRoutes);
+const corsOptions = {
+    origin: ['https://uber-clone-jet-nine.vercel.app'], // Replace with your frontend's deployed URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies to be sent
+};
 
 
 
